27053d55ffa1973b765172943a93460fbf9a1397
Chrk.dev <39009519+chrk-Dev@users.noreply.github.com>
2023-07-02 16:15:07 +0530

    add cross-orgin support

--- backend/src/main/java/dev/charu/movieApp/ReviewController.java
+++ backend/src/main/java/dev/charu/movieApp/ReviewController.java
@@ -7,11 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @RestController
 @RequestMapping("/api/v1/reviews")
 public class ReviewController {
@@ -19,6 +21,7 @@ public class ReviewController {
     @Autowired
     private ReviewService reviewService;
 
+@CrossOrigin(origins = "*", maxAge = 3600)
 @PostMapping
 public ResponseEntity<Review> createReview(@RequestBody Map<String,String> payload){
 
